457640517f4556d2f2dfcf2d06e4141a90c876a6
Simon Gerber <simon.gerber@vshn.ch>
2023-08-29 11:23:29 +0200

    Bootstrap PyO3 project
    This commit is the output of `maturin init` with the library code
    adjusted to match an empty implementation for Reclass.

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use pyo3::prelude::*;
+
+/// Reclass allows configuring various library behaviors
+#[pyclass]
+pub struct Reclass {
+    // TODO(sg): add config options
+}
+
+#[pymethods]
+impl Reclass {
+    #[new]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[pymodule]
+fn reclass_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+    // Register the top-level `Reclass` Python class which is used to configure the library
+    m.add_class::<Reclass>()?;
+    Ok(())
+}
