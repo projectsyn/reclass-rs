d2d8ba4a36f00348f5324d3ccf76c33eafc4c44a
Simon Gerber <simon.gerber@vshn.ch>
2024-02-20 17:27:51 +0100

    Add setter for `ignore_class_notfound_regexp()` in the Python interface

--- tests/test_ignore_class_notfound_regexp.py
+++ tests/test_ignore_class_notfound_regexp.py
@@ -14,7 +14,7 @@ def test_ignore_regexp_render_n1():
     assert n1 is not None
 
 
-def test_ignore_regexp_render_n1():
+def test_ignore_regexp_render_n2():
     r = reclass_rs.Reclass.from_config(
         "./tests/inventory-class-notfound-regexp", "reclass-config.yml"
     )
@@ -24,3 +24,14 @@ def test_ignore_regexp_render_n1():
         ValueError, match="Error while rendering n2: Class foo not found"
     ):
         n2 = r.nodeinfo("n2")
+
+
+def test_ignore_regexp_update_config_render_n2():
+    r = reclass_rs.Reclass.from_config(
+        "./tests/inventory-class-notfound-regexp", "reclass-config.yml"
+    )
+    r.set_ignore_class_notfound_regexp([".*"])
+    assert r.config.ignore_class_notfound_regexp == [".*"]
+
+    n2 = r.nodeinfo("n2")
+    assert n2 is not None
