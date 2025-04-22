235b5f0d0cb698d167b05138473ce8de89e0987b
Simon Gerber <simon.gerber@vshn.ch>
2025-04-22 19:13:49 +0200

    Draft impl for parsing and evaluating inventory queries
    Currently untested and some bits NYI

--- src/invqueries/mod.rs
+++ src/invqueries/mod.rs
@@ -1,16 +1,145 @@
 use anyhow::{anyhow, Result};
 
 use parser::parse_query;
+use serde_yaml::Number;
 
 use crate::types::{Mapping, Value};
 
 mod parser;
 
-struct Expression {}
+#[derive(Debug)]
+enum Expression {
+    Expr(Test, Vec<(Operator, Test)>),
+}
+
+impl Expression {
+    fn evaluate(&self, exports: &Mapping, ignore_errors: bool) -> Result<bool> {
+        match self {
+            Self::Expr(o, rem) => {
+                let res = o.evaluate(exports, ignore_errors)?;
+                for (op, operation) in rem {
+                    match op {
+                        Operator::And => res && operation.evaluate(exports, ignore_errors)?,
+                        Operator::Or => res || operation.evaluate(exports, ignore_errors)?,
+                        _ => unreachable!("unexpected test op"),
+                    };
+                }
+                Ok(res)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum QueryOption {
+    AllEnvs,
+    IgnoreErrors,
+}
+
+#[derive(Debug)]
+enum Operator {
+    Eq,
+    Neq,
+    And,
+    Or,
+}
+
+#[derive(Debug)]
+enum Test {
+    Eq(Item, Item),
+    Neq(Item, Item),
+}
+
+impl Test {
+    fn make(op: Operator, lhs: Item, rhs: Item) -> Result<Self> {
+        match op {
+            Operator::Eq => Ok(Self::Eq(lhs, rhs)),
+            Operator::Neq => Ok(Self::Neq(lhs, rhs)),
+            _ => Err(anyhow!("Unexpected operator {op:?} in Test::make")),
+        }
+    }
+
+    fn evaluate(&self, exports: &Mapping, ignore_errors: bool) -> Result<bool> {
+        match self {
+            Self::Eq(a, b) => a.eval_eq(b, exports, ignore_errors),
+            Self::Neq(a, b) => a.eval_neq(b, exports, ignore_errors),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Item {
+    Integer(i64),
+    Real(f64),
+    Obj(String),
+}
+
+impl Item {
+    fn value(&self, exports: &Mapping, ignore_errors: bool) -> Result<Option<Value>> {
+        match self {
+            Self::Obj(k) => {
+                if let Some((ktype, kpath)) = k.split_once(":") {
+                    let mut kparts = kpath.split(":");
+                    let k0 = kparts.next().ok_or(anyhow!("expected at least one key"))?;
+                    match ktype {
+                        "exports" => {
+                            let v = exports.get(&k0.into());
+                            if ignore_errors && v.is_none() {
+                                return Ok(None);
+                            }
+                            let mut v = v.ok_or(anyhow!("{k} doesn't exist"))?;
+                            while let Some(k) = kparts.next() {
+                                let n = v.get(&k.into());
+                                if ignore_errors && n.is_none() {
+                                    return Ok(None);
+                                }
+                                v = n.ok_or(anyhow!(
+                                    "Failed to get export {kpath}, {k} doesn't exist"
+                                ))?;
+                            }
+                            Ok(Some(v.clone()))
+                        }
+                        "self" => todo!("self references in exports NYI"),
+                        _ => Err(anyhow!("Unexpected lookup type {ktype}")),
+                    }
+                } else {
+                    let v = k.to_lowercase();
+                    // true or false literals
+                    match &v[..] {
+                        "true" => Ok(Some(Value::Bool(true))),
+                        "false" => Ok(Some(Value::Bool(true))),
+                        _ => Err(anyhow!("Unexpected literal {k}")),
+                    }
+                }
+            }
+            &Self::Integer(n) => Ok(Some(Value::Number(Number::from(n)))),
+            &Self::Real(n) => Ok(Some(Value::Number(Number::from(n)))),
+        }
+    }
+
+    fn eval_eq(&self, other: &Self, exports: &Mapping, ignore_errors: bool) -> Result<bool> {
+        // TODO(sg): figure out how to properly propagate missing values on `ignore_errors`
+        // what's the semantics of ignore_errors, do we silently skip the output for failing
+        // lookups?
+        let sv = self.value(exports, ignore_errors)?;
+        let ov = other.value(exports, ignore_errors)?;
+        Ok(sv == ov)
+    }
+
+    fn eval_neq(&self, other: &Self, exports: &Mapping, ignore_errors: bool) -> Result<bool> {
+        let sv = self.value(exports, ignore_errors)?;
+        let ov = other.value(exports, ignore_errors)?;
+        Ok(sv != ov)
+    }
+}
 
 pub(crate) struct Query {
     qstr: String,
-    expr: Expression,
+    var: Option<String>,
+    expr: Option<Expression>,
+    // TODO(sg): figure out what these do and implement them
+    all_envs: bool,
+    ignore_errors: bool,
 }
 
 impl Query {
@@ -19,6 +148,46 @@ impl Query {
     }
 
     pub(crate) fn resolve(&self, exports: &Mapping) -> Result<Value> {
-        Ok(Value::Literal(self.qstr.clone()))
+        if let Some(var) = &self.var {
+            let o = Item::Obj(var.clone());
+            let mut v = Mapping::new();
+            for (n, n_exports) in exports.as_map() {
+                let n_exports = n_exports.as_mapping().ok_or(anyhow!(
+                    "expected exports to be mapping for {n}, got {}",
+                    n_exports.variant()
+                ))?;
+                if let Some(e) = self.expr.as_ref() {
+                    //  TODO(sg): do we resolve exports in expr with only our own exports? or with all
+                    //  of them?
+                    if !e.evaluate(n_exports, self.ignore_errors)? {
+                        continue;
+                    }
+                }
+                let n_v = o.value(n_exports, self.ignore_errors)?;
+                if let Some(n_v) = n_v {
+                    v.insert(n.clone(), n_v)?;
+                }
+            }
+            Ok(Value::Mapping(v))
+        } else {
+            // for queries without a value, we return all nodes for which the expression evaluates
+            // to true.
+            let mut v = vec![];
+            for (n, n_exports) in exports.as_map() {
+                let n_exports = n_exports.as_mapping().ok_or(anyhow!(
+                    "expected exports to be mapping for {n}, got {}",
+                    n_exports.variant()
+                ))?;
+                if let Some(e) = self.expr.as_ref() {
+                    //  TODO(sg): do we resolve exports in expr with only our own exports? or with all
+                    //  of them?
+                    if !e.evaluate(n_exports, self.ignore_errors)? {
+                        continue;
+                    }
+                }
+                v.push(n.clone());
+            }
+            Ok(Value::Sequence(v))
+        }
     }
 }
--- src/invqueries/parser.rs
+++ src/invqueries/parser.rs
@@ -1,48 +1,145 @@
-use anyhow::Result;
-use nom::bytes::complete::{tag, tag_no_case};
-use nom::IResult;
+use anyhow::{anyhow, Result};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, tag_no_case},
+    character::complete::{digit1, none_of},
+    combinator::{all_consuming, map, map_res},
+    multi::{many0, many1},
+    number::complete::double,
+    sequence::{preceded, tuple},
+    IResult,
+};
 
-use super::{Expression, Query};
+use super::{Expression, Item, Operator, Query, QueryOption, Test};
 
 fn sign(input: &str) -> IResult<&str, &str> {
     tag("-")(input)
 }
 
-fn number(input: &str) -> IResult<&str, &str> {
-    todo!()
+fn integer(input: &str) -> IResult<&str, Item> {
+    map(
+        alt((
+            map_res(preceded(sign, digit1), |n| {
+                i64::from_str_radix(n, 10).map(|v| -v)
+            }),
+            map_res(digit1, |n| i64::from_str_radix(n, 10)),
+        )),
+        |i| Item::Integer(i),
+    )(input)
 }
 
-fn dpoint(input: &str) -> IResult<&str, &str> {
-    tag(".")(input)
+fn real(input: &str) -> IResult<&str, Item> {
+    map(double, |d| Item::Real(d))(input)
 }
 
-fn ignore_errors(input: &str) -> IResult<&str, &str> {
-    tag_no_case("+IgnoreErrors")(input)
+fn ignore_errors(input: &str) -> IResult<&str, QueryOption> {
+    map(tag_no_case("+IgnoreErrors"), |_| QueryOption::IgnoreErrors)(input)
 }
 
-fn all_envs(input: &str) -> IResult<&str, &str> {
-    tag_no_case("+AllEnvs")(input)
+fn all_envs(input: &str) -> IResult<&str, QueryOption> {
+    map(tag_no_case("+AllEnvs"), |_| QueryOption::AllEnvs)(input)
 }
 
-fn eq(input: &str) -> IResult<&str, &str> {
-    tag("==")(input)
+fn eq(input: &str) -> IResult<&str, Operator> {
+    map(tag("=="), |_| Operator::Eq)(input)
 }
 
-fn neq(input: &str) -> IResult<&str, &str> {
-    tag("!=")(input)
+fn neq(input: &str) -> IResult<&str, Operator> {
+    map(tag("!="), |_| Operator::Neq)(input)
 }
 
-fn eand(input: &str) -> IResult<&str, &str> {
-    tag_no_case("AND")(input)
+fn and(input: &str) -> IResult<&str, Operator> {
+    map(tag_no_case("AND"), |_| Operator::And)(input)
 }
 
-fn eor(input: &str) -> IResult<&str, &str> {
-    tag_no_case("OR")(input)
+fn or(input: &str) -> IResult<&str, Operator> {
+    map(tag_no_case("OR"), |_| Operator::Or)(input)
+}
+
+fn options(input: &str) -> IResult<&str, Vec<QueryOption>> {
+    many0(alt((ignore_errors, all_envs)))(input)
+}
+
+fn operator_test(input: &str) -> IResult<&str, Operator> {
+    alt((eq, neq))(input)
+}
+
+fn operator_logical(input: &str) -> IResult<&str, Operator> {
+    alt((and, or))(input)
+}
+
+fn begin_if(input: &str) -> IResult<&str, &str> {
+    tag_no_case("IF")(input)
+}
+
+fn obj(input: &str) -> IResult<&str, Item> {
+    map(many1(none_of(" \t")), |cs| {
+        Item::Obj(cs.iter().collect::<String>())
+    })(input)
+}
+
+fn expritem(input: &str) -> IResult<&str, Item> {
+    alt((integer, real, obj))(input)
+}
+
+fn single_test(input: &str) -> IResult<&str, Test> {
+    map_res(tuple((expritem, operator_test, expritem)), |(a, op, b)| {
+        Test::make(op, a, b)
+    })(input)
+}
+
+fn additional_test(input: &str) -> IResult<&str, (Operator, Test)> {
+    tuple((operator_logical, single_test))(input)
+}
+
+fn expr_var(input: &str) -> IResult<&str, (Option<String>, Option<Expression>)> {
+    map_res(all_consuming(obj), |o| match o {
+        Item::Obj(v) => Ok((Some(v), None)),
+        _ => Err(anyhow!("expr_var should only match Item::Obj, got {o:?}")),
+    })(input)
+}
+
+fn expr_test(input: &str) -> IResult<&str, (Option<String>, Option<Expression>)> {
+    map_res(
+        all_consuming(tuple((obj, begin_if, single_test, many0(additional_test)))),
+        |(v, _, t, ts)| {
+            let Item::Obj(var) = v else {
+                return Err(anyhow!("Expected value to be an Item::Obj, got {v:?}"));
+            };
+            let expr = Expression::Expr(t, ts);
+            Ok((Some(var), Some(expr)))
+        },
+    )(input)
+}
+
+fn expr_list_test(input: &str) -> IResult<&str, (Option<String>, Option<Expression>)> {
+    map(
+        all_consuming(tuple((begin_if, single_test, many0(additional_test)))),
+        |(_, t, ts)| {
+            let expr = Expression::Expr(t, ts);
+            (None, Some(expr))
+        },
+    )(input)
+}
+
+fn line(input: &str) -> IResult<&str, (Vec<QueryOption>, (Option<String>, Option<Expression>))> {
+    tuple((options, alt((expr_test, expr_var, expr_list_test))))(input)
 }
 
 pub(super) fn parse_query(s: &str) -> Result<Query> {
+    let (uncons, (opts, (var, expr))) =
+        line(s).map_err(|e| anyhow!("While parsing inventory query: {e}"))?;
+    if uncons != "" {
+        return Err(anyhow!("Parsing inventory query didn't consume '{uncons}'"));
+    }
+
+    let all_envs = opts.contains(&QueryOption::AllEnvs);
+    let ignore_errors = opts.contains(&QueryOption::IgnoreErrors);
     Ok(Query {
         qstr: s.to_owned(),
-        expr: Expression {},
+        var,
+        expr,
+        all_envs,
+        ignore_errors,
     })
 }
